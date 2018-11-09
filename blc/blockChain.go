0b4af6c7f51c5f9d93ffb9a75da3b601aa1c425e
AlexBruceLu <lu_1000_yu@163.com>
2018-11-09 21:36:52 +0800

    增加数据库，数据持久化，更改添加区块链的方法

--- blc/blockChain.go
+++ blc/blockChain.go
@@ -1,17 +1,95 @@
 package blc
 
+import (
+	"fmt"
+	"log"
+
+	"github.com/bolt"
+)
+
+const BlockDB = "blockChain.db"
+const BlockBucket = "blocks"
+
 type BlockChain struct {
-	Blocks []*Block
+	Tip []byte // 最后一个区块的哈希
+	// Blocks []*Block
+	DB *bolt.DB
 }
 
 // 创建带有创世区块的BlockChain
 func NewBlockChain() *BlockChain {
-	return &BlockChain{[]*Block{NewGenesisBlock()}}
+	//return &BlockChain{[]*Block{NewGenesisBlock()}}
+	// 1. 创建或打开数据库
+	// 2. db.update
+	var tip []byte
+	db, err := bolt.Open(BlockDB, 0600, nil)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	err = db.Update(func(tx *bolt.Tx) error {
+		bucket := tx.Bucket([]byte(BlockBucket))
+		if bucket == nil {
+			fmt.Println("No found BlockBucket, Creating a new bucket")
+			genesis := NewGenesisBlock()
+			bucket, err = tx.CreateBucket([]byte(BlockBucket))
+			if err != nil {
+				log.Panic(err)
+			}
+			err = bucket.Put(genesis.Hash, genesis.Serialize())
+			if err != nil {
+				log.Panic(err)
+			}
+			err = bucket.Put([]byte("1"), genesis.Hash)
+			if err != nil {
+				log.Panic(err)
+			}
+			tip = genesis.Hash
+
+		} else {
+			tip = bucket.Get([]byte("1"))
+		}
+		return nil
+	})
+	if err != nil {
+		log.Panic(err)
+	}
+
+	return &BlockChain{tip, db}
+
 }
 
 // 新增区块
 func (blockChain *BlockChain) AddBlock(data string) {
-	preBlock := blockChain.Blocks[len(blockChain.Blocks)-1]
-	newBlock := NewBlock(data,preBlock.Hash)
-	blockChain.Blocks = append(blockChain.Blocks,newBlock)
+	//	preBlock := blockChain.Blocks[len(blockChain.Blocks)-1]
+	//	newBlock := NewBlock(data, preBlock.Hash)
+	//	blockChain.Blocks = append(blockChain.Blocks, newBlock)
+	newBlock := NewBlock(data, blockChain.Tip)
+	// upodate 数据
+	err := blockChain.DB.Update(func(tx *bolt.Tx) error {
+
+		bucket := tx.Bucket([]byte(BlockBucket))
+		if bucket == nil {
+
+			fmt.Println("NO found blockchain. Pls. check!")
+			return nil
+
+		}
+
+		err := bucket.Put(newBlock.Hash, newBlock.Serialize())
+		if err != nil {
+			log.Panic(err)
+		}
+		err = bucket.Put([]byte("1"), newBlock.Hash)
+		if err != nil {
+			log.Panic(err)
+		}
+		blockChain.Tip = newBlock.Hash
+		return nil
+
+	})
+	if err != nil {
+		log.Panic(err)
+	}
+
 }
--- main.go
+++ main.go
@@ -3,24 +3,26 @@ package main
 import (
 	"fmt"
 	"test/blc/blc"
-	"time"
 )
 
 func main() {
 	blockChain := blc.NewBlockChain()
-	blockChain.AddBlock("to xiaohong 20 BTC")
-	blockChain.AddBlock("to xiaohong 20 BTC")
-	blockChain.AddBlock("to xiaohong 20 BTC")
+	fmt.Println(blockChain)
+	fmt.Printf("tip : %x\n", blockChain.Tip)
+	blockChain.AddBlock("send 20 BTC to xiaohong")
+	fmt.Printf("tip : %x\n", blockChain.Tip)
+	//blockChain := blc.NewBlockChain()
+	//blockChain.AddBlock("to xiaohong 20 BTC")
 
-	for _, block := range blockChain.Blocks {
-		byte1 := block.Serialize()
-		fmt.Println(byte1)
-		block = blc.Deserialize(byte1)
-		fmt.Printf("Data: %s\n", string(block.Data))
-		fmt.Printf("PrevHash: %x\n", block.PrevHash)
-		fmt.Printf("Hash: %x\n", block.Hash)
-		fmt.Printf("nonce: %x\n", block.Nonce)
-		fmt.Printf("DTimeStamp: %v\n", time.Unix(block.TimeStamp, 0).Format("2006-01-02 15:04:05"))
-		fmt.Printf("\n\n")
-	}
+	//for _, block := range blockChain.Blocks {
+	//	byte1 := block.Serialize()
+	//	fmt.Println(byte1)
+	//	block = blc.Deserialize(byte1)
+	//	fmt.Printf("Data: %s\n", string(block.Data))
+	//	fmt.Printf("PrevHash: %x\n", block.PrevHash)
+	//	fmt.Printf("Hash: %x\n", block.Hash)
+	//	fmt.Printf("nonce: %x\n", block.Nonce)
+	//	fmt.Printf("DTimeStamp: %v\n", time.Unix(block.TimeStamp, 0).Format("2006-01-02 15:04:05"))
+	//	fmt.Printf("\n\n")
+	//}
 }
